6b157e2bebd6750dda7e0e19322c6538935c41aa
Richard <>
2018-09-11 15:36:24 +0800

    added very basic functionality

--- coffeehawk.js
+++ coffeehawk.js
@@ -12,11 +12,17 @@
 
 
 /* 
+    When customer enters line, count how long it takes for the customer to reach the cashier
     When customer approaches cashier, count the time it takes for cashier to initialize first input in cash register 
     start a timer when cashier first inputs order, measure the time between each button press, and the total number of times the back button or clear button was pressed
     measure the time between when the order is finalized between customer and cashier and customer successfully pays
     measure time between when order is sent from cash register to reception in "kitchen" area
-    measure time between order reception in "kitchen" area and the time the drink gets to the pick up counter
+    for each type of drink compare the statistical norm for how long it takes to make each drink to how long it takes to make the drink and put on the pick-up counter
     every 5 seconds measure how many people are in line and how many leave before reaching the cashier
-
 */
+let TimeinLine = 6;
+let TimeatCashier = 10;
+let TimeatBarista = 5;
+console.log(TimeatCashier + TimeatBarista + TimeinLine)
+
+
